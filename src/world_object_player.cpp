91e8aec93ee7675022d6012da293b779dcfc782a
Chris Lewis <chris@chrislewisdev.com>
2024-07-30 17:03:14 +1000

    Fix tiles for older maps, and fix portal colliders

--- src/world_object_player.cpp
+++ src/world_object_player.cpp
@@ -32,8 +32,7 @@ namespace sp {
         if (bn::keypad::a_pressed()) {
             auto collider = bn::rect(position.x.integer(), position.z.integer(), 32, 32);
             for (auto portal : world_state.get_current_zone().get_portals()) {
-                BN_LOG("Portal data:", portal.x, ",", portal.y, ",", portal.width, ",", portal.height);
-                auto rect = bn::rect(portal.x + portal.width/2, portal.y + portal.height/2, portal.width, portal.height);
+                auto rect = bn::rect(portal.x + portal.width/2, portal.y - portal.height/2, portal.width, portal.height);
 
                 if (collider.intersects(rect)) {
                     world_state.queue_zone_change(portal);

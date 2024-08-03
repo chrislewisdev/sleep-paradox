275554e707f230a0ea7afabdf1846ac510b8c6a0
Chris Lewis <chris@chrislewisdev.com>
2024-08-03 15:02:42 +1000

    Use new title screen art

--- include/scene_title.h
+++ include/scene_title.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#include "bn_vector.h"
-#include "bn_sprite_ptr.h"
+#include "bn_regular_bg_ptr.h"
 
 #include "scene.h"
 #include "scene_manager.h"
 
 namespace sp {
+
     class scene_title : public scene {
         private:
             sp::scene_manager& scene_manager;
-            bn::vector<bn::sprite_ptr, 16> text_sprites;
+            bn::regular_bg_ptr bg;
         public:
             scene_title(sp::scene_manager& _scene_manager);
             void update();

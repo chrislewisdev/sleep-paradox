684f7e940c42db53bb78c51fd1468da97fe34938
Chris Lewis <chris@chrislewisdev.com>
2024-08-18 23:30:04 +1000

    Add ending screen

--- include/scene_end.h
+++ include/scene_end.h
@@ -2,6 +2,7 @@
 
 #include "bn_vector.h"
 #include "bn_sprite_ptr.h"
+#include "bn_regular_bg_ptr.h"
 
 #include "scene.h"
 #include "scene_manager.h"
@@ -12,6 +13,8 @@ namespace sp {
             sp::scene_manager& scene_manager;
             sp::world_state& world_state;
             bn::vector<bn::sprite_ptr, 16> text_sprites;
+            bn::regular_bg_ptr bg;
+
         public:
             scene_end(sp::scene_manager& scene_manager, sp::world_state& world_state);
             void update();
--- src/scene_end.cpp
+++ src/scene_end.cpp
@@ -3,16 +3,16 @@
 #include "bn_sprite_text_generator.h"
 
 #include "common_variable_8x16_sprite_font.h"
+#include "bn_regular_bg_items_end.h"
 
 namespace sp {
     scene_end::scene_end(sp::scene_manager& _scene_manager, sp::world_state& _world_state)
-        : scene_manager(_scene_manager), world_state(_world_state)
+        : scene_manager(_scene_manager), world_state(_world_state), bg(bn::regular_bg_items::end.create_bg(0, 0))
     {
         bn::sprite_text_generator text_gen(common::variable_8x16_sprite_font);
         text_gen.set_center_alignment();
 
-        text_gen.generate(0, -8, "The end!", text_sprites);
-        text_gen.generate(0, 8, "Thanks for playing :3", text_sprites);
+        text_gen.generate(70, -60, "The end.", text_sprites);
 
         world_state.cleanup();
     }
